5650254a39816e9b49d97e51e668ab1cf45bba72
Ahmet <89451731+ahmetcakr@users.noreply.github.com>
2021-10-19 14:19:17 +0300

    Veritabanı
    Giriş için veritabanı bağlantısı oluşturuldu.

--- UludagOtomasyon2/Form1_Giris.cs
+++ UludagOtomasyon2/Form1_Giris.cs
@@ -7,29 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
 
 namespace UludagOtomasyon2
 {
     public partial class Form1_Giris : Form
     {
         Form2_Duyurular form2_Duyurular = new Form2_Duyurular();
+
+
         public Form1_Giris()
         {
             InitializeComponent();
         }
 
+
+        // VERİTABANI BAĞLANTI KISAYOLLARI
+                SqlConnection con;
+                SqlCommand cmd;
+                SqlDataReader dr;
+      
+
+
         private void btn_Giris_Click(object sender, EventArgs e)
         {
-           if (txt_KullanıcıAdi.Text == "ahmet" && txt_Sifre.Text == "1234")
-           {
-            form2_Duyurular.Show();
-            this.Hide();
-            }
-             else
+
+            string sorgu = "SELECT * FROM KullaniciAdiVeSifre where KullaniciAdi=@KullaniciAdi AND Sifre=@Sifre";
+            con = new SqlConnection("Data Source=AHMETSPC;Initial Catalog=UludagUniversity;Integrated Security=true");
+            cmd = new SqlCommand(sorgu, con);
+            cmd.Parameters.AddWithValue("@KullaniciAdi", txt_KullanıcıAdi.Text);
+            cmd.Parameters.AddWithValue("@Sifre", txt_Sifre.Text);
+            con.Open();
+
+            dr = cmd.ExecuteReader();
+
+            if (dr.Read())
             {
-                MessageBox.Show("Kullanıcı Adı veya Şifreniz Yanlış");
+                MessageBox.Show("Başarıyla Giriş Yapılmıştır");
+                form2_Duyurular.Show();
+                this.Hide();
             }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı veya Şifre Yanlış");
+            }
+
+            con.Close();
         }
+
+
 
         private void Form1_Giris_FormClosing(object sender, FormClosingEventArgs e)
         {

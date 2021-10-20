3ab7d17b0b429720087d191f253e42a7a68907ee
Ahmet <89451731+ahmetcakr@users.noreply.github.com>
2021-10-20 21:08:58 +0300

    Yönetici Form
    Yönetici formu ve öğrenci ekleme kısmı eklendi.

---
+++ UludagOtomasyon2/Form17_YoneticiAnaSayfa.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace UludagOtomasyon2
+{
+    partial class Form17_YoneticiAnaSayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form17_YoneticiAnaSayfa));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.txt_ogrIsimEkle = new System.Windows.Forms.TextBox();
+            this.lbl_ogrIsimEkle = new System.Windows.Forms.Label();
+            this.txt_ogrSoyisimEkle = new System.Windows.Forms.TextBox();
+            this.lbl_ogrSoyisimEkle = new System.Windows.Forms.Label();
+            this.txt_ogrNoEkle = new System.Windows.Forms.TextBox();
+            this.lbl_ogrNoEkle = new System.Windows.Forms.Label();
+            this.btn_ogrEkle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Location = new System.Drawing.Point(1, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(754, 562);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btn_ogrEkle);
+            this.tabPage1.Controls.Add(this.lbl_ogrNoEkle);
+            this.tabPage1.Controls.Add(this.lbl_ogrSoyisimEkle);
+            this.tabPage1.Controls.Add(this.lbl_ogrIsimEkle);
+            this.tabPage1.Controls.Add(this.txt_ogrNoEkle);
+            this.tabPage1.Controls.Add(this.txt_ogrSoyisimEkle);
+            this.tabPage1.Controls.Add(this.txt_ogrIsimEkle);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(746, 536);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ogrenci Ekle";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(746, 536);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ogrenci Düzenle";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(746, 536);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Ogretmen Ekle";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(746, 536);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Ogretmen Düzenle";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(746, 536);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Sınıf Ekle";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(746, 536);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Sınıf Düzenle";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // txt_ogrIsimEkle
+            // 
+            this.txt_ogrIsimEkle.Location = new System.Drawing.Point(202, 69);
+            this.txt_ogrIsimEkle.Multiline = true;
+            this.txt_ogrIsimEkle.Name = "txt_ogrIsimEkle";
+            this.txt_ogrIsimEkle.Size = new System.Drawing.Size(150, 32);
+            this.txt_ogrIsimEkle.TabIndex = 0;
+            // 
+            // lbl_ogrIsimEkle
+            // 
+            this.lbl_ogrIsimEkle.AutoSize = true;
+            this.lbl_ogrIsimEkle.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ogrIsimEkle.Location = new System.Drawing.Point(131, 75);
+            this.lbl_ogrIsimEkle.Name = "lbl_ogrIsimEkle";
+            this.lbl_ogrIsimEkle.Size = new System.Drawing.Size(67, 20);
+            this.lbl_ogrIsimEkle.TabIndex = 1;
+            this.lbl_ogrIsimEkle.Text = "İsim    =";
+            // 
+            // txt_ogrSoyisimEkle
+            // 
+            this.txt_ogrSoyisimEkle.Location = new System.Drawing.Point(202, 107);
+            this.txt_ogrSoyisimEkle.Multiline = true;
+            this.txt_ogrSoyisimEkle.Name = "txt_ogrSoyisimEkle";
+            this.txt_ogrSoyisimEkle.Size = new System.Drawing.Size(150, 32);
+            this.txt_ogrSoyisimEkle.TabIndex = 0;
+            // 
+            // lbl_ogrSoyisimEkle
+            // 
+            this.lbl_ogrSoyisimEkle.AutoSize = true;
+            this.lbl_ogrSoyisimEkle.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ogrSoyisimEkle.Location = new System.Drawing.Point(106, 114);
+            this.lbl_ogrSoyisimEkle.Name = "lbl_ogrSoyisimEkle";
+            this.lbl_ogrSoyisimEkle.Size = new System.Drawing.Size(92, 20);
+            this.lbl_ogrSoyisimEkle.TabIndex = 1;
+            this.lbl_ogrSoyisimEkle.Text = "Soyisim    =";
+            // 
+            // txt_ogrNoEkle
+            // 
+            this.txt_ogrNoEkle.Location = new System.Drawing.Point(202, 145);
+            this.txt_ogrNoEkle.Multiline = true;
+            this.txt_ogrNoEkle.Name = "txt_ogrNoEkle";
+            this.txt_ogrNoEkle.Size = new System.Drawing.Size(150, 32);
+            this.txt_ogrNoEkle.TabIndex = 0;
+            // 
+            // lbl_ogrNoEkle
+            // 
+            this.lbl_ogrNoEkle.AutoSize = true;
+            this.lbl_ogrNoEkle.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ogrNoEkle.Location = new System.Drawing.Point(77, 152);
+            this.lbl_ogrNoEkle.Name = "lbl_ogrNoEkle";
+            this.lbl_ogrNoEkle.Size = new System.Drawing.Size(121, 20);
+            this.lbl_ogrNoEkle.TabIndex = 1;
+            this.lbl_ogrNoEkle.Text = "Öğrenci No    =";
+            // 
+            // btn_ogrEkle
+            // 
+            this.btn_ogrEkle.Location = new System.Drawing.Point(411, 103);
+            this.btn_ogrEkle.Name = "btn_ogrEkle";
+            this.btn_ogrEkle.Size = new System.Drawing.Size(88, 44);
+            this.btn_ogrEkle.TabIndex = 2;
+            this.btn_ogrEkle.Text = "EKLE";
+            this.btn_ogrEkle.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(563, 534);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form17_YoneticiAnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 563);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(767, 602);
+            this.MinimumSize = new System.Drawing.Size(767, 602);
+            this.Name = "Form17_YoneticiAnaSayfa";
+            this.Text = "Yönetici Ana Sayfası";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form17_YoneticiAnaSayfa_FormClosing);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btn_ogrEkle;
+        private System.Windows.Forms.Label lbl_ogrNoEkle;
+        private System.Windows.Forms.Label lbl_ogrSoyisimEkle;
+        private System.Windows.Forms.Label lbl_ogrIsimEkle;
+        private System.Windows.Forms.TextBox txt_ogrNoEkle;
+        private System.Windows.Forms.TextBox txt_ogrSoyisimEkle;
+        private System.Windows.Forms.TextBox txt_ogrIsimEkle;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ UludagOtomasyon2/Form17_YoneticiAnaSayfa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UludagOtomasyon2
+{
+    public partial class Form17_YoneticiAnaSayfa : Form
+    {
+        public Form17_YoneticiAnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void Form17_YoneticiAnaSayfa_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
